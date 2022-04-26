9bb2aab93fab3e018ef4f4a856dc05a3144b2bcb
Frederick Lopez <mtester010@gmail.com>
2022-04-26 16:05:30 -0500

    Add images for year 2017

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ let previousYear = currentYear -1;
 const currentYearEl = document.querySelector('.current-year');
 const futureYearEl = document.querySelector('.future-year');
 const previousYearEl = document.querySelector('.previous-year');
+const imageContainer = document.querySelector('.image-container');
 
 futureYearEl.addEventListener('click', () => {
   currentYear++;
@@ -34,8 +35,10 @@ function printYear() {
 function checkMinYear() {
   if (currentYear === 2017) {
     previousYearEl.style.display = 'none';
+    imageContainer.style.display = 'block';
   } else {
     previousYearEl.style.display = 'block';
+    imageContainer.style.display = 'none';
   }
 }
 
