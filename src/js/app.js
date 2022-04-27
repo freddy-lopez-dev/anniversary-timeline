2b6fcb4033f0ba915b9a7a36d78a5f83ad1153eb
Frederick Lopez <mtester010@gmail.com>
2022-04-27 11:40:41 -0500

    Add 2020 pictures

--- src/js/app.js
+++ src/js/app.js
@@ -7,20 +7,30 @@ const previousYearEl = document.querySelector('.previous-year');
 const imageContainer17 = document.querySelector('.image-container2017');
 const imageContainer18 = document.querySelector('.image-container2018');
 const imageContainer19 = document.querySelector('.image-container2019');
+const imageContainer20 = document.querySelector('.image-container2020');
 
-futureYearEl.addEventListener('click', () => {
-  currentYear++;
-  futureYear++;
-  previousYear++;
-  printYear();
-})
+setTimeout(nextYear, 3000);
+setTimeout(prevYear, 3000);
 
-previousYearEl.addEventListener('click', () => {
-  currentYear--;
-  futureYear--;
-  previousYear--;
-  printYear();
-})
+function nextYear() {
+  futureYearEl.addEventListener('click', () => {
+    currentYear++;
+    futureYear++;
+    previousYear++;
+    printYear();
+  })
+}
+
+function prevYear() {
+  previousYearEl.addEventListener('click', () => {
+    currentYear--;
+    futureYear--;
+    previousYear--;
+    printYear();
+  })
+}
+
+
 
 printYear();
 
@@ -57,8 +67,18 @@ function checkMinYear() {
     previousYearEl.style.display = 'block';
     imageContainer18.style.animation = 'fadeOut 3s';
     setTimeout(hide2018, 2500);
+    imageContainer20.style.animation = 'fadeOut 3s';
+    setTimeout(hide2020, 2500);
     imageContainer19.style.animation = 'fadeIn 3s';
     imageContainer19.style.display = 'block';
+  }
+
+  if (currentYear === 2020) {
+    previousYearEl.style.display = 'block';
+    imageContainer19.style.animation = 'fadeOut 3s';
+    setTimeout(hide2019, 2500);
+    imageContainer20.style.animation = 'fadeIn 3s';
+    imageContainer20.style.display = 'block';
   }
 }
 
@@ -80,4 +100,8 @@ function hide2018() {
 
 function hide2019() {
   imageContainer19.style.display = 'none';
+}
+
+function hide2020() {
+  imageContainer20.style.display = 'none';
 }
